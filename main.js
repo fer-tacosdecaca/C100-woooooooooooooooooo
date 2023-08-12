503b24231f3aead534481b9baf7a48c6778c09f0
fer-tacosdecaca <130688038+fer-tacosdecaca@users.noreply.github.com>
2023-08-11 20:01:49 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -15,16 +15,46 @@ recognition.onresult = function(event) {
    
        Textbox.innerHTML = Content;
        console.log(Content);
-         
+       
+      if(Content == "chupa el baño"){
+        speak()
+      }
+
+
+       
    }
 
 
 
    
+
+camera = document.getElementById("camára");
+Webcam.set({ 
+    width:360, 
+    height:250, 
+    image_format : 'jpeg', 
+    jpeg_quality:90 
+});
+
 function speak() {
- var kk = window.speechSynthesis
- speak_data = document.getElementById("textbox").value;
- var utterThis = new SpeechSynthesisUtterance(speak_data);
- kk.speak(utterThis);
+    var kk = window.speechSynthesis
+    speak_data = document.getElementById("textbox").value;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    kk.speak(utterThis);
+    Webcam.attach(camera)
+    setTimeout(function() { takeselfie(); save(); }, 5000);
+    
+
+   }
+function takeserfie(){
+    Webcam.snap(function(data_uri)
+    {
+        document.getElementById("foto").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>'
+    });
+}7
+function save() { link = document.getElementById("link"); image = document.getElementById("selfie_image").src ;
+link.href= image; link.click(); 
 }
 
+
+       
